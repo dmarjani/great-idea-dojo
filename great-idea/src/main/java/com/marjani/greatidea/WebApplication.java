980ac4e03d15e5a2a78cf63a026d2618a5921516
dorian <dormarjani@gmail.com>
2021-03-11 14:55:51 +0100

    changed in ordet to deloy externaly on tomcat the web app

--- great-idea/src/main/java/com/marjani/greatidea/WebApplication.java
+++ great-idea/src/main/java/com/marjani/greatidea/WebApplication.java
@@ -2,10 +2,19 @@ package com.marjani.greatidea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebApplication {
-    public static void main(String[] args) throws Exception {
+public class WebApplication extends SpringBootServletInitializer {
+    
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebApplication.class);
+	}
+	
+	public static void main(String[] args) throws Exception {
         SpringApplication.run(WebApplication.class, args);
     }
+	
 }
--- great-idea/target/greatidea-0.0.1-SNAPSHOT/resources/js/welcome.js
+++
@@ -1,5 +0,0 @@
-(function () {
-	$('#ideasTable a.like').on('click', function() {
-		console.log('ideasTable onClick() value ' + this.value);
-	});	
-})();
